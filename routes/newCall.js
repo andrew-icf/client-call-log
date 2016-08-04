5b1cd2d05b44ed35f1983f2642f25f9d053376fd
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2016-08-03 21:33:07 -0600

    Adding landing page and new call page

--- routes/newCall.js
+++ routes/newCall.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login' });
+  res.render('newCall');
 });
 
 module.exports = router;
