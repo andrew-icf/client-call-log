62b6a226cc1e27d2c4fafd7fce0d62b2d4e60ec7
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2016-08-03 21:54:11 -0600

    adding new patient page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var newCall = require('./routes/newCall');
+var newPatient = require('./routes/newPatient');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/newCall', newCall);
+app.use('/newPatient', newPatient);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/newPatient.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('newPatient');
+});
+
+module.exports = router;
